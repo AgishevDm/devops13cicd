51a3b3ad8dc4465437f6d801209e6c88902e3aa0
AgishevDM <109414135+AgishevDm@users.noreply.github.com>
2025-03-21 23:26:26 +0300

    Update test_application.py

--- server/test_application.py
+++ server/test_application.py
@@ -2,9 +2,9 @@ import pytest
 from application import TestMe
 
 def test_take_five():
-    """Проверяет, что метод take_five возвращает 5."""
+    """ Проверяет, что метод take_five возвращает 5 """
     assert TestMe().take_five() == 5
 
 def test_port():
-    """Проверяет, что порт сервера равен 8000."""
+    """ Проверяет, что порт сервера равен 8000 """
     assert TestMe().port() == 8000
